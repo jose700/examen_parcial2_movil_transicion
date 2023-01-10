8469e03bc2a5e714e461fa2bdde6dafb091fac8f
jose700 <josecampu28@gmail.com>
2023-01-09 22:18:08 -0500

    Update bookList.dart

--- lib/services/bookList.dart
+++ lib/services/bookList.dart
@@ -1,8 +1,10 @@
 import 'package:examen_parcial2/services/networking.dart';
 
-//
+// creamos una constante y en ella añadimos la api a la cual se consumira los servicios, en mi caso una de libros
 const bookAPIUrl = 'http://skunkworks.ignitesol.com:8000/books/?mime_type=image%2Fjpeg';
 // const bookAPIUrl = 'https://www.abibliadigital.com.br/api/books';
+
+//creamos una clase para mostrar las diferentes categorias
 class CatalogService {
   Future<dynamic> getBooks(String category) async {
     var url = '$bookAPIUrl&topic=$category';
@@ -10,7 +12,7 @@ class CatalogService {
     var catalogData = await networkHelper.getData();
     return catalogData;
   }
-
+   //tambien creamos una función en caso de requerir realizar alguna búsqueda(no es necesaria la implementación ya que en este proyecto no se añadio dicha funcionalidad)
   Future<dynamic> searchBooks(String category, String searchQuery) async {
     List<String> list = searchQuery.split(' ');
     searchQuery = list.join('%20');
